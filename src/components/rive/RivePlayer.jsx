c631bf40675065ccc5d4a6180d2a26b1cbe9bcd4
roborbob <robertjames1804@gmail.com>
2023-02-23 23:59:50 +0000

    chore: mona lisa

--- src/components/rive/RivePlayer.jsx
+++ src/components/rive/RivePlayer.jsx
@@ -2,17 +2,46 @@
 import React from "react";
 import { useRive } from "@rive-app/react-canvas";
 
-export default function RivePlayer({ animation, artboard }) {
+export const RivePlayerOne = ({ animation, artboard }) => {
   const { RiveComponent: RiveComponentPlayer } = useRive({
     src: animation,
     artboard: artboard,
-    // stateMachines: "state",
     autoplay: "true",
   });
 
   return (
-    <div className="w-[200px] h-[200px] hover:cursor-pointer ">
+    <div className="w-full md:w-[200px] h-[300px] md:h-[200px] hover:cursor-pointer ">
       <RiveComponentPlayer />
     </div>
   );
-}
+};
+
+export const RivePlayerTwo = ({ animation, artboard, stateMachine }) => {
+  const { RiveComponent: RiveComponentPlayer } = useRive({
+    src: animation,
+    artboard: artboard,
+    stateMachines: stateMachine,
+    autoplay: "true",
+  });
+
+  return (
+    <div className=" w-full md:w-[200px] h-[200px] hover:cursor-pointer ">
+      <RiveComponentPlayer />
+    </div>
+  );
+};
+
+export const RivePlayerThree = ({ animation, artboard, stateMachine }) => {
+  const { RiveComponent: RiveComponentPlayer } = useRive({
+    src: animation,
+    artboard: artboard,
+    stateMachines: stateMachine,
+    autoplay: "true",
+  });
+
+  return (
+    <div className="w-full md:w-[600px] h-[600px] hover:cursor-pointer ">
+      <RiveComponentPlayer />
+    </div>
+  );
+};
