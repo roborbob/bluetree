e759718a95d8ddb4a6bf0db88df2f48261cffce1
roborbob <robertjames1804@gmail.com>
2023-04-07 10:35:06 +0100

    chore: remove post handler

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -2,32 +2,9 @@
 
 import React from "react";
 
-const data = {
-  email: "dev.blueforest@gmail.com",
-  data: {
-    contact_name: "Robert James",
-    contact_email: "robertjames1804@gmail.com",
-    contact_message: "Test message from bluetree.dev",
-  },
-};
 export default function Form() {
   const HandleClick = async () => {
-    const options = {
-      method: "POST",
-      body: JSON.stringify(data),
-      headers: { "Content-Type": "application/json" },
-    };
-    try {
-      const response = await fetch(
-        "https://emailapi-eqc5em4yea-oa.a.run.app/mail",
-        options
-      );
-      if (response.ok) {
-        console.log("Success");
-      }
-    } catch (err) {
-      console.error(err);
-    }
+    console.log("click");
   };
   return (
     <div>
